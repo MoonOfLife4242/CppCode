21109e4f94c2730fc793002ec925762dbe4c8ebf
MoonOfLife4242 <rettanegash1990@gmail.com>
2022-03-04 17:39:09 +0300

    swapping two intiger values without using temporary value

---
+++ swapWithoutTem.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+ // int arr[] = {23,34,54,78};
+ // int n = sizeof(arr)/sizeof(arr[0]);
+
+ // for (int i = 0; i <= 2; ++i)
+ // {
+ // 	arr[i] = arr[i+1];
+ // 	arr[i+1] = arr[i-1];
+ // 	arr[i] = arr[i-1];
+
+ // 	cout<<"swapped data" <<arr[i]<<endl;
+ // }
+ // for (int i = 0; i < sizeof(arr); ++i)
+ // {
+ // 	cout<<arr[i];
+ // }
+
+	int x,y;
+	cout<<"Enter the first number " <<endl;
+	cin>>x;
+	cout<<"Enter the second number";
+	cin>>y;
+		cout<<"the second number is "<<x<<endl;
+		cout<<"the second number is "<<y<<endl;
+
+
+	x = x+y;
+	y = x - y;
+	x = x - y;
+	cout<<" now the first value is : "<<x<<endl;
+	cout<<"now the second valueis : " <<y<<endl;
+ return 0;
+	
+}
