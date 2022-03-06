ff91dd4db37ab89ec34e24f73f2ab1359b28045c
MoonOfLife4242 <rettanegash1990@gmail.com>
2022-03-06 14:38:05 +0300

    cpp encapsulation example

---
+++ encapsulation.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+// The salary attribute is private, which have restricted access.
+
+// declare private variable
+class Employe {
+	private:	
+		int salary;
+
+// set the value to modify by set method
+	public:
+		void setSalary(int s) {
+			salary = s;
+		}
+		// get the value to access by get method
+		int getSalary() {
+			return salary;
+		}
+};
+int main() {
+
+	Employe Em;
+
+	Em.setSalary(8000);
+	int salary = Em.getSalary();
+
+	cout<<salary;
+	
+	return 0;
+}
